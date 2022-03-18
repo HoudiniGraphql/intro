fecde0c967d4ce521436e8853f915623313c83a4
Alec Aivazis <alec@aivazis.com>
2022-03-18 13:22:34 -0700

    add api_url and ws_url env var

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import houdini from 'houdini-preprocess';
 import path from 'path';
+import { replaceCodePlugin } from 'vite-plugin-replace';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -20,7 +21,21 @@ const config = {
 				fs: {
 					allow: ['.']
 				}
-			}
+			},
+			plugins: [
+				replaceCodePlugin({
+					replacements: [
+						{
+							from: 'API_URL',
+							to: JSON.stringify(process.env.API_URL || 'http://localhost:4000/graphql')
+						},
+						{
+							from: 'WS_URL',
+							to: JSON.stringify(process.env.WS_URL || 'ws://localhost:4000/graphql')
+						}
+					]
+				})
+			]
 		}
 	}
 };
