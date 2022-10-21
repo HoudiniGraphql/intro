b0c37cbe31abfd92a67124cb95f0a1afc784c652
Alec Aivazis <alec@aivazis.com>
2022-10-21 13:18:41 -0700

    update (#8)

---
+++ src/routes/[...id]/+page.js
@@ -0,0 +1,13 @@
+export function SpeciesInfoVariables({ params }) {
+	// if we were given an id, convert the string to a number
+	const id = params.id ? parseInt(params.id) : 1;
+
+	// check that the id falls between 1 and 151
+	if (id < 1 || id > 151) {
+		return this.error(400, 'id must be between 1 and 151');
+	}
+
+	return {
+		id
+	};
+}
--- src/routes/[...id]/+page.svelte
+++ src/routes/[...id]/+page.svelte
@@ -1,19 +1,3 @@
-<script context="module">
-	export function SpeciesInfoVariables({ params }) {
-		// if we were given an id, convert the string to a number
-		const id = params.id ? parseInt(params.id) : 1;
-
-		// check that the id falls between 1 and 151
-		if (id < 1 || id > 151) {
-			return this.error(400, 'id must be between 1 and 151');
-		}
-
-		return {
-			id
-		};
-	}
-</script>
-
 <script>
 	import {
 		Panel,
@@ -29,10 +13,10 @@
 		UpButton,
 		DownButton
 	} from '~/components';
-	import { query, paginatedQuery, graphql, mutation, subscription } from '$houdini';
+	import { graphql } from '$houdini';
 	import { navigating } from '$app/stores';
 
-	const { data, loadNextPage, pageInfo } = paginatedQuery(graphql`
+	const SpeciesInfo = graphql`
 		query SpeciesInfo($id: Int!) {
 			species(id: $id) {
 				name
@@ -51,18 +35,18 @@
 				...SpriteInfo
 			}
 		}
-	`);
+	`;
 
 	// this is a second query in the same route
-	const { data: favoriteData } = query(graphql`
+	const Favorites = graphql`
 		query Favorites {
 			favorites @list(name: "FavoriteSpecies") {
 				...FavoritePreview
 			}
 		}
-	`);
+	`;
 
-	const toggleFavorite = mutation(graphql`
+	const toggleFavorite = graphql`
 		mutation ToggleFavorite($id: Int!) {
 			toggleFavorite(id: $id) {
 				species {
@@ -70,11 +54,13 @@
 				}
 			}
 		}
-	`);
+	`;
 
 	let moveIndex = 0;
 	$: hasPrevMove = moveIndex > 0;
-	$: hasNextMove = moveIndex < $data.species.moves.edges.length - 1 || $pageInfo.hasNextPage;
+	$: hasNextMove =
+		moveIndex < $SpeciesInfo.data.species.moves.edges.length - 1 ||
+		$SpeciesInfo.pageInfo.hasNextPage;
 
 	navigating.subscribe(() => {
 		moveIndex = 0;
@@ -82,7 +68,10 @@
 
 	const loadNextMove = async () => {
 		// if we haven't already seen this page
-		if (!$data.species.moves.edges[moveIndex + 1] && $pageInfo.hasNextPage) {
+		if (
+			!$SpeciesInfo.data.species.moves.edges[moveIndex + 1] &&
+			$SpeciesInfo.pageInfo.hasNextPage
+		) {
 			// load the next page of data
 			await loadNextPage();
 		}
@@ -108,7 +97,7 @@
 </script>
 
 <FavoritesContainer>
-	{#each $favoriteData.favorites as favorite}
+	{#each $Favorites.data.favorites as favorite}
 		<FavoritePreview species={favorite} />
 	{:else}
 		<p>No Favorites Selected</p>
@@ -118,37 +107,47 @@
 <Container>
 	<Panel slot="left">
 		<Display id="species-name">
-			{$data.species.name}
-			<span>no.{$data.species.id}</span>
+			{$SpeciesInfo.data.species.name}
+			<span>no.{$SpeciesInfo.data.species.id}</span>
 		</Display>
-		<Sprite id="species-sprite" species={$data.species} />
+		<Sprite id="species-sprite" species={$SpeciesInfo.data.species} />
 		<Display id="species-flavor_text">
-			{$data.species.flavor_text}
+			{$SpeciesInfo.data.species.flavor_text}
 		</Display>
-		<button id="favorite" on:click={() => toggleFavorite({ id: $data.species.id })}>
-			<Icon name="star" id="favorite-star" fill={$data.species.favorite ? 'gold' : 'lightgrey'} />
+		<button id="favorite" on:click={() => toggleFavorite({ id: $SpeciesInfo.data.species.id })}>
+			<Icon
+				name="star"
+				id="favorite-star"
+				fill={$SpeciesInfo.data.species.favorite ? 'gold' : 'lightgrey'}
+			/>
 		</button>
 	</Panel>
 	<Panel slot="right">
 		<div id="species-evolution-chain">
-			{#each $data.species.evolution_chain as form, i}
+			{#each $SpeciesInfo.data.species.evolution_chain as form, i}
 				<SpeciesPreview species={form} number={i + 1} />
 			{/each}
 			<!-- if there are less than three species in the chain, leave a placeholder behind -->
-			{#each Array.from({ length: 3 - $data.species.evolution_chain?.length }) as _, i}
-				<SpeciesPreviewPlaceholder number={$data.species.evolution_chain.length + i + 1} />
+			{#each Array.from({ length: 3 - $SpeciesInfo.data.species.evolution_chain?.length }) as _, i}
+				<SpeciesPreviewPlaceholder
+					number={$SpeciesInfo.data.species.evolution_chain.length + i + 1}
+				/>
 			{/each}
 		</div>
 		<div id="move-summary">
-			<MoveDisplay move={$data.species.moves.edges[moveIndex].node} />
+			<MoveDisplay move={$SpeciesInfo.data.species.moves.edges[moveIndex].node} />
 			<div id="move-controls">
 				<UpButton disabled={!hasPrevMove} on:click={loadPrevMove} />
 				<DownButton disabled={!hasNextMove} on:click={loadNextMove} />
 			</div>
 		</div>
 		<nav>
-			<a href={$data.species.id - 1} disabled={$data.species.id <= 1}> previous </a>
-			<a href={$data.species.id + 1} disabled={$data.species.id >= 151}> next </a>
+			<a href={$SpeciesInfo.data.species.id - 1} disabled={$SpeciesInfo.data.species.id <= 1}>
+				previous
+			</a>
+			<a href={$SpeciesInfo.data.species.id + 1} disabled={$SpeciesInfo.data.species.id >= 151}>
+				next
+			</a>
 		</nav>
 	</Panel>
 </Container>
