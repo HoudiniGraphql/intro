fee6cdef1150ac51b6903928d60fbf2917ca2a56
Warre Dujardin <warre.dujardin@gmail.com>
2025-07-08 06:45:15 +0200

    Fix Typescript, CORS errors (#19)
    * add preprocessing for typescript support
    
    * point client to a relative address

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 import adapter from '@sveltejs/adapter-auto'
 import path from 'path'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
 		alias: {
