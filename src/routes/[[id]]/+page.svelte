add3284c1731c5966b3c98ccb90bbcf6b7ebb4a6
Alec Aivazis <alec@aivazis.com>
2026-06-15 17:42:31 -0700

    fix fragment reactivity and pagination, update houdini deps

--- src/routes/[[id]]/+page.svelte
+++ src/routes/[[id]]/+page.svelte
@@ -35,65 +35,69 @@
 		{/each}
 	</FavoritesContainer>
 
-	<Container>
-		{#snippet left()}
-			<Panel>
-				<button
-					id="favorite"
-					onclick={() => toggleFavorite.mutate({ id: $Info.data.species.id })}
-				>
-					<Icon
-						name="star"
-						id="favorite-star"
-						fill={$Info.data.species.favorite ? 'gold' : 'lightgrey'}
-					/>
-				</button>
-				<Display id="species-name">
-					{$Info.data.species.name}
-					<span>no.{$Info.data.species.id}</span>
-				</Display>
-				<Sprite id="species-sprite" species={$Info.data.species} />
-				<Display id="species-flavor_text">
-					{$Info.data.species.flavor_text}
-				</Display>
-			</Panel>
-		{/snippet}
-
-		{#snippet right()}
-			<Panel>
-				<div id="species-evolution-chain">
-					{#each $Info.data.species.evolution_chain as form, i}
-						<SpeciesPreview species={form} number={i + 1} />
-					{/each}
-					{#each Array.from({ length: 3 - $Info.data.species.evolution_chain.length }) as _, i}
-						<SpeciesPreviewPlaceholder number={$Info.data.species.evolution_chain.length + i + 1} />
-					{/each}
-				</div>
-
-				<div id="move-summary">
-					<MoveDisplay move={$Info.data.species.moves.edges[0].node} />
-					<div id="move-controls">
-						<UpButton
-							disabled={!$Info.data.species.moves.pageInfo.hasPreviousPage}
-							onclick={() => Info.loadPreviousPage()}
-						/>
-						<DownButton
-							disabled={!$Info.data.species.moves.pageInfo.hasNextPage}
-							onclick={() => Info.loadNextPage()}
-						/>
-					</div>
-				</div>
-
-				<nav>
-					<a
-						href={$Info.data.species.id > 1 ? `/${$Info.data.species.id - 1}` : undefined}
-						class={$Info.data.species.id <= 1 ? 'disabled' : undefined}
+	{#key $Info.data.species.id}
+		<Container>
+			{#snippet left()}
+				<Panel>
+					<button
+						id="favorite"
+						onclick={() => toggleFavorite.mutate({ id: $Info.data.species.id })}
 					>
-						previous
-					</a>
-					<a href="/{$Info.data.species.id + 1}">next</a>
-				</nav>
-			</Panel>
-		{/snippet}
-	</Container>
+						<Icon
+							name="star"
+							id="favorite-star"
+							fill={$Info.data.species.favorite ? 'gold' : 'lightgrey'}
+						/>
+					</button>
+					<Display id="species-name">
+						{$Info.data.species.name}
+						<span>no.{$Info.data.species.id}</span>
+					</Display>
+					<Sprite id="species-sprite" species={$Info.data.species} />
+					<Display id="species-flavor_text">
+						{$Info.data.species.flavor_text}
+					</Display>
+				</Panel>
+			{/snippet}
+
+			{#snippet right()}
+				<Panel>
+					<div id="species-evolution-chain">
+						{#each $Info.data.species.evolution_chain as form, i}
+							<SpeciesPreview species={form} number={i + 1} />
+						{/each}
+						{#each Array.from({ length: 3 - $Info.data.species.evolution_chain.length }) as _, i}
+							<SpeciesPreviewPlaceholder number={$Info.data.species.evolution_chain.length + i + 1} />
+						{/each}
+					</div>
+
+					<div id="move-summary">
+						{#key $Info.data.species.moves.pageInfo.startCursor}
+							<MoveDisplay move={$Info.data.species.moves.edges[0].node} />
+						{/key}
+						<div id="move-controls">
+							<UpButton
+								disabled={!$Info.data.species.moves.pageInfo.hasPreviousPage}
+								onclick={() => Info.loadPreviousPage()}
+							/>
+							<DownButton
+								disabled={!$Info.data.species.moves.pageInfo.hasNextPage}
+								onclick={() => Info.loadNextPage()}
+							/>
+						</div>
+					</div>
+
+					<nav>
+						<a
+							href={$Info.data.species.id > 1 ? `/${$Info.data.species.id - 1}` : undefined}
+							class={$Info.data.species.id <= 1 ? 'disabled' : undefined}
+						>
+							previous
+						</a>
+						<a href="/{$Info.data.species.id + 1}">next</a>
+					</nav>
+				</Panel>
+			{/snippet}
+		</Container>
+	{/key}
 {/if}
